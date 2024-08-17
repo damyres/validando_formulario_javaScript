be47e5e03dabab0ac07d4e9ba60bd9cbc66ccef2
Damyres <damyresmaciell@gmail.com>
2024-08-17 11:58:44 -0300

    Criar função para validar CPF em campo na tela, passando caracteres ou não

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
+import ehUmCpf from "./validarCpf.js"
 const camposDoFormulario = document.querySelectorAll("[required")
 //console.log(camposDoFormulario)
 
 camposDoFormulario.forEach((campo) => {
     campo.addEventListener("blur", () => verficaCampo(campo))
-   
 })
 
-function verficaCampo(campo){
-    
+function verficaCampo(campo) {
+    if (campo.name == "cpf" && campo.value.length >= 11) {
+        ehUmCpf(campo)
+    }
 
 }
