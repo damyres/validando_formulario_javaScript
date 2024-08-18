ff4f44e9c1e5fa02460eaf75fbb6cd95aa93116e
Damyres <damyresmaciell@gmail.com>
2024-08-18 16:33:07 -0300

    Criar função de envio das informações em tel

--- js/script.js
+++ js/script.js
@@ -2,6 +2,26 @@ import ehUmCpf from "./validarCpf.js"
 import ehMaiorDeIdade from "./validaIdade.js"
 
 const camposDoFormulario = document.querySelectorAll("[required")
+const formulario = document.querySelector("[data-formulario]")
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const listaRespostas = {
+        "nome": e.target.elements["nome"].value,
+        "email": e.target.elements["email"].value,
+        "rg": e.target.elements["rg"].value,
+        "cpf": e.target.elements["cpf"].value,
+        "aniversario": e.target.elements["aniversario"].value,
+    }
+
+    localStorage.setItem("cadastro", JSON.stringify(listaRespostas))
+
+    window.location.href = '../pages/abrir-conta-form-2.html'
+
+   //console.log(formulario)
+
+})
 //console.log(camposDoFormulario)
 
 camposDoFormulario.forEach((campo) => {
@@ -52,7 +72,7 @@ const mensagens = {
 function verficaCampo(campo) {
     let mensagem = "";
     campo.setCustomValidity('')
- 
+
     if (campo.name == "cpf" && campo.value.length >= 11) {
         ehUmCpf(campo)
     }
