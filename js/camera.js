a0fecf899937207966b4c4f88e4f6751576b4c21
Damyres <damyresmaciell@gmail.com>
2024-08-18 20:39:16 -0300

    Salvar a foto e exibindo a confirmação de sucesso

--- js/camera.js
+++ js/camera.js
@@ -3,9 +3,11 @@ const campoCamera = document.querySelector("[data-camera]")
 const video = document.querySelector("[data-video]")
 const botaoTirarFoto = document.querySelector("[data-tirar-foto]")
 const canvas = document.querySelector("[data-video-canvas]")
-const menssagem = document.querySelector("[data-messagem]")
+const menssagem = document.querySelector("[data-mensagem]")
+const botaoEnviarFoto = document.querySelector("[data-enviar]")
 
 let imagemURL = "";
+
 botaoIniciarCamera.addEventListener("click", async function () {
     const inciarVideo = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
 
@@ -16,12 +18,22 @@ botaoIniciarCamera.addEventListener("click", async function () {
     video.srcObject = inciarVideo;
 })
 
-botaoTirarFoto.addEventListener("click", function () {
+botaoTirarFoto.addEventListener("click", function() {
     canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
 
-    imagemURL = canvas.toDataURL("image/jpeg");
+    imagemURL = canvas.toDataURL("image/png");
 
     campoCamera.style.display = "none";
 
     menssagem.style.display = "block";
+})
+
+botaoEnviarFoto.addEventListener("click", () => {
+    const receberDadosExistentes = localStorage.getItem("cadastro")
+    const converterRetorno = JSON.parse(receberDadosExistentes)
+
+    converterRetorno.imagem = imagemURL
+    localStorage.setItem('cadastro', JSON.stringify(converterRetorno));
+    window.location.href = "../pages/abrir-conta-form-3.html";
+
 })
